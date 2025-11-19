79e25c66ecfec0b79157c476fffe799058ae0a6a
letChar <alezbozman@gmail.com>
2025-11-19 23:28:57 +0300

    Add project files: Vue.js game with Tailwind CSS

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center">
+    <Game />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Game from './components/Game.vue'
+</script>
+
+<style>
+</style>
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
